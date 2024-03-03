aac84821c85e2784abdca03cd7b72d77ce561282
Shatohin Dmitriy <greatsun@yandex.ru>
2024-03-03 15:10:31 +0500

    .

---
+++ version.py
@@ -0,0 +1,7 @@
+def get_version():
+    version = '030320241522'
+    filename = "https://raw.githubusercontent.com/Kevinjareczek/CSCI490/master/traininglabels.txt"
+    with open(filename) as f:
+        print('')
+
+    return version
